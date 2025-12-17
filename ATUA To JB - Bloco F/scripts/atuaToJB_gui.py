b09e6b8964ea8c82089c3edf9ca533be640af2b1
Iury Prates @RControl <ti@rcontrolbr.com.br>
2025-12-17 14:02:57 -0400

    add extension file condition

--- ATUA To JB - Bloco F/scripts/atuaToJB_gui.py
+++ ATUA To JB - Bloco F/scripts/atuaToJB_gui.py
@@ -10,17 +10,66 @@ from atuaToJb import processar_arquivo
 
 VERSAO = "1.0"
 
+#------------------------------------
+#   Valida se o arquivo possui layout ATUA esperado.
+#   Retorna True se válido, levanta Exception se inválido.
+#-------------------------------------
+
+def validar_layout_arquivo(caminho_arquivo):
+    extensoes_permitidas = (".seq", ".txt")
+
+    if not caminho_arquivo.lower().endswith(extensoes_permitidas):
+        raise Exception(
+            "Arquivo inválido. \n\n"
+            "Selecione um arquivo com extensão (.seq) ou (.txt)."
+        )
+
+    with open(caminho_arquivo, "r", encoding="latin-1") as f:
+        linhas = f.readlines()
+
+    if not linhas:
+        raise Exception("O arquivo está vazio.")
+
+    possui_00 = False
+    possui_06 = False
+
+    for linha in linhas:
+        linha = linha.strip()
+
+        if linha.startswith("00;"):
+            possui_00 = True
+        elif linha.startswith("06;"):
+            possui_06 = True
+
+        if possui_00 and possui_06:
+            break
+
+    if not possui_00:
+        raise Exception("Layout inválido: nenhuma linha do tipo 00 encontrada.")
+
+    if not possui_06:
+        raise Exception("Layout inválido: nenhuma linha do tipo 06 encontrada.")
+
+    return True
 
 def selecionar_arquivo():
     arquivo = filedialog.askopenfilename(
         title="Selecione o arquivo ATUA (.seq)",
-        filetypes=[("Arquivos SEQ", "*.seq"), ("Arquivos Texto", "*.txt")]
+        filetypes=[
+            ("Todos os arquivos", "*.*"),
+            ("Arquivos SEQ", "*.seq"), 
+            ("Arquivos Texto", "*.txt")
+            
+            ]
     )
 
     if not arquivo:
         return
 
     try:
+
+        validar_layout_arquivo(arquivo)
+
         # Interface em modo processamento
         botao.config(state="disabled")
         status_label.config(text="Processando arquivo, aguarde...")
@@ -63,7 +112,7 @@ janela.resizable(False, False)
 label = tk.Label(
     janela,
     text="Conversão ATUA → JB\n\n"
-         "Selecione o arquivo (.seq) de origem",
+         "Selecione o arquivo (.seq / .txt) de origem",
     font=("Arial", 11),
     justify="center"
 )
